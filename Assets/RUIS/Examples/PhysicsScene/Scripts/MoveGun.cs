1c588e43bf844bca12f2ad4e1f1f096a99336aec
mmatvein <mmatvein@20bc764e-ed91-41fa-8628-e371393b6745>
2012-10-25 15:47:39 +0000

    added gun to physicsscene
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/trunk@28 20bc764e-ed91-41fa-8628-e371393b6745

---
+++ Assets/RUIS/Examples/PhysicsScene/Scripts/MoveGun.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using System.Collections;
+
+public class MoveGun : MonoBehaviour {
+    RUISPSMoveController moveController;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawnSpot;
+
+    public float bulletSpeed = 1.0f;
+
+    public float shootingInterval = 0.3f;
+    float timeSinceLastShot = 0;
+
+    void Awake()
+    {
+        moveController = GetComponent<RUISPSMoveController>();
+    }
+
+	void Update () {
+        timeSinceLastShot += Time.deltaTime;
+
+        if (moveController.triggerValue > 0.85 && timeSinceLastShot >= shootingInterval)
+        {
+            timeSinceLastShot = 0;
+            GameObject bullet = Instantiate(bulletPrefab, bulletSpawnSpot.position, bulletSpawnSpot.rotation) as GameObject;
+            bullet.rigidbody.AddForce(bulletSpawnSpot.forward * bulletSpeed, ForceMode.VelocityChange);
+
+            Destroy(bullet, 5);
+        }
+	}
+}
