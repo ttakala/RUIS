107e11c6d2764f3c1ee44d798aadb3e00c82e866
mmatvein <mmatvein@20bc764e-ed91-41fa-8628-e371393b6745>
2013-01-21 12:22:29 +0000

    moved stuff around a bit
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/trunk@56 20bc764e-ed91-41fa-8628-e371393b6745

---
+++ Assets/RUIS/Examples/Football/Scripts/ReelInSelection.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+
+public class ReelInSelection : MonoBehaviour {
+    public float reelSpeed = 1.0f;
+    
+    RUISPSMoveWand psMoveController;
+    RUISWandSelector wandSelector;
+
+    RUISSelectable selection;
+
+    bool wasClampedToCertainDistance;
+    float distanceClampedTo;
+
+    float currentDistance = 1;
+
+	void Awake () {
+        psMoveController = GetComponent<RUISPSMoveWand>();
+        wandSelector = GetComponent<RUISWandSelector>();
+	}
+
+    void Update () {
+        if (!wandSelector.Selection && selection)
+        {
+            selection.clampToCertainDistance = wasClampedToCertainDistance;
+            selection.distanceToClampTo = distanceClampedTo;
+        }
+
+        if (!wandSelector.Selection || wandSelector.positionSelectionGrabType != RUISWandSelector.SelectionGrabType.AlongSelectionRay)
+        {
+            selection = null;
+            return;
+        }
+
+        if(!selection){
+            selection = wandSelector.Selection;
+            
+            wasClampedToCertainDistance = selection.clampToCertainDistance;
+            selection.clampToCertainDistance = true;
+
+            distanceClampedTo = selection.distanceToClampTo;
+
+            currentDistance = 1;
+        }
+
+        float currentDistanceChange = ((1 - psMoveController.triggerValue) - currentDistance) * Time.deltaTime * reelSpeed;
+        currentDistance += currentDistanceChange;
+        selection.distanceToClampTo = currentDistance * selection.DistanceFromSelectionRayOrigin;
+	}
+}
