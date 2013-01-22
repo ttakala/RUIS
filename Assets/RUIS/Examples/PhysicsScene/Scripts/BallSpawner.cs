c67a7868eeddd09d67e748bbb30173ef26cb7080
mmatvein <mmatvein@20bc764e-ed91-41fa-8628-e371393b6745>
2013-01-22 15:57:41 +0000

    fixed a couple of bugs
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/trunk@65 20bc764e-ed91-41fa-8628-e371393b6745

--- Assets/RUIS/Examples/PhysicsScene/Scripts/BallSpawner.cs
+++ Assets/RUIS/Examples/PhysicsScene/Scripts/BallSpawner.cs
@@ -11,7 +11,7 @@ public class BallSpawner : MonoBehaviour {
 	
 	void Update () {
 	    for(int i = 0; i < 4; i++){
-            if (psMoveWrapper.moveConnected[i] && psMoveWrapper.WasPressed(i, PSMoveWrapper.CIRCLE))
+            if (psMoveWrapper.sphereVisible[i] && psMoveWrapper.WasPressed(i, PSMoveWrapper.CIRCLE))
             {
                 Instantiate(ball, transform.position, transform.rotation);
             }
