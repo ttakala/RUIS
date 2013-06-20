ffba614d5ca37c7adbb2bb136daecaea412f2055
mmatvein <mmatvein@20bc764e-ed91-41fa-8628-e371393b6745>
2013-06-20 13:35:53 +0000

    the controllable character is now able to crouch
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/trunk@112 20bc764e-ed91-41fa-8628-e371393b6745

--- Assets/RUIS/Scripts/CharacterController/RUISCharacterStabilizingCollider.cs
+++ Assets/RUIS/Scripts/CharacterController/RUISCharacterStabilizingCollider.cs
@@ -1,16 +1,32 @@
 using UnityEngine;
 using System.Collections;
 
+//assumes kinect ground is at Y = 0
+[RequireComponent(typeof(CapsuleCollider))]
 public class RUISCharacterStabilizingCollider : MonoBehaviour {
     public RUISPlainSkeletonController skeletonController;
 
+    RUISSkeletonManager skeletonManager;
+    int playerId;
+
+    private CapsuleCollider collider;
+
+    public float maxHeightChange = 5f;
+    public float maxPositionChange = 10f;
+
 	void Start () {
-	
+        skeletonManager = skeletonController.skeletonManager;
+        playerId = skeletonController.playerId;
+
+        collider = GetComponent<CapsuleCollider>();
 	}
 	
 	void FixedUpdate () {
-        Vector3 newPos = skeletonController.transform.localPosition;
-        //newPos.y = 0;
-        transform.localPosition = newPos;
+        Vector3 torsoPos = skeletonManager.skeletons[playerId].torso.position;
+        Vector3 newPos = torsoPos;
+        newPos.y = torsoPos.y / 2;
+
+        collider.height = Mathf.Lerp(collider.height, torsoPos.y - collider.radius, maxHeightChange * Time.fixedDeltaTime);
+        transform.localPosition = Vector3.Lerp(transform.localPosition, newPos, maxPositionChange * Time.fixedDeltaTime);
 	}
 }
