4e648d672f446971b4891d1728525f2e30f4e983
tmtakala <tmtakala@20bc764e-ed91-41fa-8628-e371393b6745>
2014-05-08 21:26:42 +0000

    ObliqueFrustum shadows seem to work. Editing multiple RUISCamera components at the same time works now, but undo & redo still cause problems.
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/trunk@232 20bc764e-ed91-41fa-8628-e371393b6745

--- Assets/RUIS/Editor/RUISCameraEditor.cs
+++ Assets/RUIS/Editor/RUISCameraEditor.cs
@@ -8,7 +8,6 @@ public class RUISCameraEditor : Editor
 {
 	
 	SerializedProperty fovFactor;
-	SerializedProperty aspFactor;
 
     SerializedProperty near;
     SerializedProperty far;
@@ -16,12 +15,17 @@ public class RUISCameraEditor : Editor
     SerializedProperty verticalFOV;
     SerializedProperty cullingMask;
 
-	RUISCamera camera;
+//	RUISCamera camera;
 	Camera leftEye;
 	Camera rightEye;
 
+	private bool updateNear;
+	private bool updateFar;
+	private bool updateFieldOfView;
+
     void OnEnable()
     {
+
         near = serializedObject.FindProperty("near");
         far = serializedObject.FindProperty("far");
 
@@ -30,7 +34,7 @@ public class RUISCameraEditor : Editor
         
         cullingMask = serializedObject.FindProperty("cullingMask");
 
-        camera = target as RUISCamera;
+//        camera = target as RUISCamera;
     }
 
     public override void OnInspectorGUI()
@@ -42,51 +46,88 @@ public class RUISCameraEditor : Editor
         EditorGUILayout.LabelField("Clipping Planes");
         EditorGUI.indentLevel++;
         EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.PropertyField(near, new GUIContent("Near", "Near clipping plane distance (affects all Camera scripts as well as OVRCamera Controller)"));
-		EditorGUILayout.PropertyField(far, new GUIContent("Far", "Far clipping plane distance (affects all Camera scripts as well as OVRCamera Controller)"));
+
+		EditorGUI.BeginChangeCheck();
+		GUI.SetNextControlName("near");
+        EditorGUILayout.PropertyField(near, new GUIContent("Near", "Near clipping plane distance (affects all child Camera scripts as well as OVRCamera Controller)"));
+		if(EditorGUI.EndChangeCheck() || ( Event.current.keyCode == KeyCode.Return && GUI.GetNameOfFocusedControl() == "near") )
+			updateNear = true;
+		else
+			updateNear = false;
+		
+		EditorGUI.BeginChangeCheck();
+		GUI.SetNextControlName("far");
+		EditorGUILayout.PropertyField(far, new GUIContent("Far", "Far clipping plane distance (affects all child Camera scripts as well as OVRCamera Controller)"));
+		if(EditorGUI.EndChangeCheck() || ( Event.current.keyCode == KeyCode.Return && GUI.GetNameOfFocusedControl() == "far") )
+			updateFar = true;
+		else
+			updateFar = false;
+
         EditorGUILayout.EndHorizontal();
         EditorGUI.indentLevel--;
         EditorGUILayout.LabelField("Fields of View");
         EditorGUI.indentLevel++;
         EditorGUILayout.BeginHorizontal();
-		horizontalFOV.floatValue = EditorGUILayout.Slider(new GUIContent("Horizontal", "Horizontal Field of View (this value is ignored if Oculus Rift is used)"), 
-		                                                  				 horizontalFOV.floatValue, 0, 179);
-		verticalFOV.floatValue = EditorGUILayout.Slider(new GUIContent("Vertical", "Vertical Field of View (this value is ignored if Oculus Rift is used)"), 
-		                                                				verticalFOV.floatValue, 0, 179);
+		
+		EditorGUI.BeginChangeCheck();
+		GUI.SetNextControlName("horizontal");
+		horizontalFOV.floatValue = EditorGUILayout.Slider(new GUIContent(  "Horizontal", "Horizontal Field of View (this value is ignored if Oculus Rift or "
+		                                                                 + "Head Tracked CAVE Display is used)"), horizontalFOV.floatValue, 0, 179);
+		if(EditorGUI.EndChangeCheck() || ( Event.current.keyCode == KeyCode.Return && GUI.GetNameOfFocusedControl() == "horizontal") )
+			updateFieldOfView = true;
+		else
+			updateFieldOfView = false;
+
+
+		verticalFOV.floatValue = EditorGUILayout.Slider(new GUIContent(  "Vertical", "Vertical Field of View (this value is ignored if Oculus Rift or "
+		                                                               + "Head Tracked CAVE Display is used)"), verticalFOV.floatValue, 0, 179);
         EditorGUILayout.EndHorizontal();
         EditorGUI.indentLevel--;
 
-		// Without this conditional (GUI.changed) the near/far values from Camera script cannot be edited individually
-		if(GUI.changed || Event.current.keyCode == KeyCode.Return)
+		if(targets != null)
 		{
-			camera.camera.nearClipPlane = near.floatValue;
-			camera.camera.farClipPlane = far.floatValue;
-			camera.camera.fieldOfView = horizontalFOV.floatValue;
-
-			near.floatValue = camera.camera.nearClipPlane;
-			far.floatValue = camera.camera.farClipPlane;
-			horizontalFOV.floatValue = camera.camera.fieldOfView;
-
-			OVRCameraController oculusCameraController = camera.gameObject.GetComponent<OVRCameraController>();
-			if(oculusCameraController)
+			foreach(RUISCamera cameraScript in targets)
 			{
-				oculusCameraController.NearClipPlane = near.floatValue;
-				oculusCameraController.FarClipPlane  = far.floatValue;
-			}
+				if(updateNear)
+					cameraScript.camera.nearClipPlane = near.floatValue;
+				if(updateFar)
+					cameraScript.camera.farClipPlane = far.floatValue;
+				if(updateFieldOfView)
+					cameraScript.camera.fieldOfView = horizontalFOV.floatValue;
 
-			if(camera.rightCamera)
-			{
-				camera.rightCamera.nearClipPlane = near.floatValue;
-				camera.rightCamera.farClipPlane = far.floatValue;
-				camera.rightCamera.fieldOfView = horizontalFOV.floatValue;
+				OVRCameraController oculusCameraController = cameraScript.gameObject.GetComponent<OVRCameraController>();
+				if(oculusCameraController)
+				{
+					if(updateNear)
+						oculusCameraController.NearClipPlane = near.floatValue;
+					if(updateFar)
+						oculusCameraController.FarClipPlane  = far.floatValue;
+				}
+
+				if(cameraScript.rightCamera)
+				{
+					if(updateNear)
+						cameraScript.rightCamera.nearClipPlane = near.floatValue;
+					if(updateFar)
+						cameraScript.rightCamera.farClipPlane = far.floatValue;
+					if(updateFieldOfView)
+						cameraScript.rightCamera.fieldOfView = horizontalFOV.floatValue;
+				}
+				
+				if(cameraScript.leftCamera)
+				{
+					if(updateNear)
+						cameraScript.leftCamera.nearClipPlane = near.floatValue;
+					if(updateFar)
+						cameraScript.leftCamera.farClipPlane = far.floatValue;
+					if(updateFieldOfView)
+						cameraScript.leftCamera.fieldOfView = horizontalFOV.floatValue;
+				}
 			}
 			
-			if(camera.leftCamera)
-			{
-				camera.leftCamera.nearClipPlane = near.floatValue;
-				camera.leftCamera.farClipPlane = far.floatValue;
-				camera.leftCamera.fieldOfView = horizontalFOV.floatValue;
-			}
+//			near.floatValue = camera.camera.nearClipPlane;
+//			far.floatValue = camera.camera.farClipPlane;
+//			horizontalFOV.floatValue = camera.camera.fieldOfView;
 		}
 
         serializedObject.ApplyModifiedProperties();
