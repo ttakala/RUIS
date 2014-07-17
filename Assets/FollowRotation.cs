dc4c58538776197e7187d27e5b05b18ef91913f8
hjheiska <hjheiska@20bc764e-ed91-41fa-8628-e371393b6745>
2014-07-17 16:50:07 +0000

    Untracked files
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/branches/heikki@249 20bc764e-ed91-41fa-8628-e371393b6745

---
+++ Assets/FollowRotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject a = GameObject.Find("a");
+		GameObject b = GameObject.Find("b");
+		GameObject vectorUp = GameObject.Find("vectorUp");
+
+		Vector3 rotationVector = a.transform.position - b.transform.position;
+		Quaternion elbowRotation = a.transform.rotation;
+
+		vectorUp.transform.rotation = Quaternion.LookRotation (elbowRotation * Vector3.up);
+		this.transform.rotation = Quaternion.LookRotation (rotationVector, elbowRotation * Vector3.up);
+
+
+	}
+}
