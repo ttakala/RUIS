47b5b4fc7d3b90434fb8f49e24c7a652cfd58999
tmtakala <tmtakala@20bc764e-ed91-41fa-8628-e371393b6745>
2015-08-08 16:52:20 +0000

    Added Mapack/Vector class
    git-svn-id: svn+ssh://vc.tml.hut.fi/srv/svn/ruis-unity/RUIS/trunk@438 20bc764e-ed91-41fa-8628-e371393b6745

---
+++ Assets/Mapack/Vector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Vector {
+	private double[] array;
+
+	/// <summary>Constructs a vector of the given length and assigns a given value to all elements.</summary>
+	/// <param name="length">Vector length.</param>
+	/// <param name="value">Value to assign to the elements.</param>
+	public Vector(int length, double value)
+	{
+		this.array = new double[length];
+		
+		for (int i = 0; i < length; i++)
+		{
+			array[i] = value;
+		}
+	}
+	
+	public int Length
+	{
+		get 
+		{ 
+			return array.Length; 
+		}
+	}
+}
