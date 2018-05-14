cf2412871969424543dca9234bc60c5fe07f0366
tmtakala <tmtakala@gmail.com>
2018-05-14 22:19:39 +0300

    Modified rotation test so that it works with OpenVR controllers or other input

--- Assets/RUIS/Scripts/Util/RotationTestingScript.cs
+++ Assets/RUIS/Scripts/Util/RotationTestingScript.cs
@@ -1,8 +1,8 @@
 /*****************************************************************************
 
 Content    :   An internal class used to test out different quaternion rotation modifications
-Authors    :   Mikael Matveinen
-Copyright  :   Copyright 2013 Tuukka Takala, Mikael Matveinen. All Rights reserved.
+Authors    :   Tuukka Takala, Mikael Matveinen
+Copyright  :   Copyright 2018 Tuukka Takala, Mikael Matveinen. All Rights reserved.
 Licensing  :   RUIS is distributed under the LGPL Version 3 license.
 
 ******************************************************************************/
@@ -10,38 +10,61 @@ Licensing  :   RUIS is distributed under the LGPL Version 3 license.
 using UnityEngine;
 using System.Collections;
 
-public class RotationTestingScript : MonoBehaviour {
+public class RotationTestingScript : MonoBehaviour 
+{
     public int controllerId = 0;
     public int flipSignsId = 0;
-    public PSMoveWrapper psMoveWrapper;
-    public GameObject[] controllers;
+//    public PSMoveWrapper psMoveWrapper;
+	public GameObject visualizer;
+	GameObject[] visualizers = new GameObject[24];
+
+	public float xIncrement = 1;
+	public float yIncrement = 1;
+
+
+	public Transform inputPose1;
+	public Transform inputPose2;
 
     private int t = 1;
     private int u = 1;
     private int v = 1;
     private int w = 1;
 
-	void Start () {
-        controllers[0].transform.rotation = Quaternion.LookRotation(Vector3.up);
+	void Start () 
+	{
+//        visualizers[0].transform.rotation = Quaternion.LookRotation(Vector3.up);
+//        psMoveWrapper.Connect();
 
-        psMoveWrapper.Connect();
+		for(int i = 0; i < 24; ++i)
+		{
+			int rowIndex = i % 4;
+			int columnIndex = i / 4;
+			visualizers[i] = Instantiate(visualizer, transform.position + new Vector3(rowIndex * xIncrement, columnIndex * yIncrement, 0), Quaternion.identity);
+		}
 	}
 
-    void OnDestroy()
-    {
-        psMoveWrapper.Disconnect(false);
-    }
+//    void OnDestroy()
+//    {
+//        psMoveWrapper.Disconnect(false);
+//    }
 
     void Update()
     {
-        if (psMoveWrapper.WasReleased(controllerId, PSMoveWrapper.CROSS))
+		if(Input.GetButtonDown("Fire1") /* psMoveWrapper.WasReleased(controllerId, PSMoveWrapper.CROSS) */)
         {
+			print("aa");
             flipSignsId++;
             if (flipSignsId >= 16)
             {
                 flipSignsId = 0;
             }
         }
+
+
+		Quaternion inputRotation = inputPose1.localRotation;
+
+		inputRotation = QuaternionFromMatrix(Matrix4x4.TRS(Vector3.zero, inputRotation, Vector3.one));
+//		print(Matrix4x4.TRS(Vector3.zero, inputRotation, Vector3.one));
 
         if (flipSignsId > 7)
             t = -1;
@@ -73,13 +96,13 @@ public class RotationTestingScript : MonoBehaviour {
                 break;
         }
 
-        float a = t * psMoveWrapper.qOrientation[controllerId].w;
-        float b = u * psMoveWrapper.qOrientation[controllerId].x;
-        float c = v * psMoveWrapper.qOrientation[controllerId].y;
-        float d = w * psMoveWrapper.qOrientation[controllerId].z;
+		float a = t * inputRotation.w; //psMoveWrapper.qOrientation[controllerId].w;
+		float b = u * inputRotation.x; //psMoveWrapper.qOrientation[controllerId].x;
+		float c = v * inputRotation.y; //psMoveWrapper.qOrientation[controllerId].y;
+		float d = w * inputRotation.z; //psMoveWrapper.qOrientation[controllerId].z;
 
         // Generate all 24 quaternion element order combinations
-        for (int i = 0; i < 24; ++i)
+        for(int i = 0; i < 24; ++i)
         {
             Quaternion quat = new Quaternion();
 
@@ -140,7 +163,23 @@ public class RotationTestingScript : MonoBehaviour {
                     quat.w = c; quat.x = b; quat.y = d; quat.z = a; break;
             }
 
-            controllers[i].transform.rotation = quat;
+            visualizers[i].transform.rotation = quat;
         }
     }
+
+
+	public Quaternion QuaternionFromMatrix(Matrix4x4 m)
+	{
+		// Source: http://answers.unity3d.com/questions/11363/converting-matrix4x4-to-quaternion-vector3.html
+		// Adapted from: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
+		Quaternion q = new Quaternion();
+		q.w = Mathf.Sqrt( Mathf.Max( 0, 1 + m[0,0] + m[1,1] + m[2,2] ) ) / 2;
+		q.x = Mathf.Sqrt( Mathf.Max( 0, 1 + m[0,0] - m[1,1] - m[2,2] ) ) / 2;
+		q.y = Mathf.Sqrt( Mathf.Max( 0, 1 - m[0,0] + m[1,1] - m[2,2] ) ) / 2;
+		q.z = Mathf.Sqrt( Mathf.Max( 0, 1 - m[0,0] - m[1,1] + m[2,2] ) ) / 2;
+		q.x *= Mathf.Sign( q.x * ( m[2,1] - m[1,2] ) );
+		q.y *= Mathf.Sign( q.y * ( m[0,2] - m[2,0] ) );
+		q.z *= Mathf.Sign( q.z * ( m[1,0] - m[0,1] ) );
+		return q;
+	}
 }
